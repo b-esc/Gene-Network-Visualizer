038a356a3d01d455e1e1c08e746589fce192962a
esc <esc@esc-qt>
2020-06-02 15:25:00 -0500

    query bug

--- client/src/Splash.jsx
+++ client/src/Splash.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useStore } from 'react-context-hook';
-import { Segment, Header, Image, Grid, Container, Menu, Responsive, Icon } from 'semantic-ui-react';
+import { Segment, Header, Image, Grid, Container, Menu, Responsive, Icon, Button, List } from 'semantic-ui-react';
 import GraphContainer from "./components/GraphContainer";
 import NodeHoverModal from "./components/NodeHoverModal";
 import MoreInfoModal from "./components/MoreInfoModal";
@@ -35,7 +35,7 @@ export default function (){
         </Container>
 
         <Icon.Group size='huge' style={{marginTop: '1em'}}>
-          <Icon loading size='big' name='spinner'/>
+          <Icon loading size='huge' name='spinner'/>
           <Icon name='dna' />
         </Icon.Group>
 
@@ -48,10 +48,14 @@ export default function (){
 
           <Header
             as='h3'
-            content='lawrence-dill plat informatics & computation lab'
-            style={{fontSize : '2em', marginTop: '1em'}}
+            content='lawrence-dill plant informatics & computation'
+            style={{fontSize : '2em', marginTop: '1em', marginBottom: '1em'}}
             />
         </Container>
+
+        <Button size='massive' color='green' inverted>
+          Enter
+        </Button>
 
       </Segment>
 
@@ -64,7 +68,83 @@ export default function (){
 
       </Header>
 
+      <Segment textAlign='center' style={{minHeight:650, padding: '0em 0em'}}>
+        <Grid celled>
+          <Grid.Row>
+            <Grid.Column width={4}>
+              <List style={{fontSize:"1.5em"}}>
+                <List.Item>
+                  This application visualizes and tabulates gene information
+                </List.Item>
+                <List.Item>
+                  The database is configurable and user provided
+                </List.Item>
+                <List.Item>
+                  See Documentation => BuildDb.go
+                </List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={4}>
+              <List style={{fontSize:"1.5em"}}>
+                <List.Item>
+                  A user may search the database for a Gene via their unique ID's,
+                  identifying names, relevant descriptional text, among other properties.
+                </List.Item>
+                <List.Item>
+                  Search results include a graph and table of X most related Genes
+                </List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={4}>
+              <List style={{fontSize:"1.5em"}}>
+                <List.Item>
+                  Relatedness is determined by user provided weight / distance from
+                  our 'root gene' (what gene was returned by the search)
+                </List.Item>
+              </List>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
 
+      <Segment
+        textAlign='right'
+        style={{minHeight:650, padding: '0.75em 0em'}}
+        vertical
+      >
+      <Grid celled>
+        <Grid.Row>
+          <Grid.Column width={11}>
+            <Image src="https://i.ibb.co/d5fQh11/image.png"></Image>
+          </Grid.Column>
+          <Grid.Column width={4}>
+            <List style={{fontSize:"1.5em"}}>
+              <List.Item>
+                This application visualizes and tabulates gene information
+              </List.Item>
+              <List.Item>
+                The database is configurable and user provided
+              </List.Item>
+              <List.Item>
+                See Documentation => BuildDb.go
+              </List.Item>
+              <List.Item>
+                A user may search the database for a Gene via their unique ID's,
+                identifying names, relevant descriptional text, among other properties.
+              </List.Item>
+              <List.Item>
+                Search results include a graph and table of X most related Genes
+              </List.Item>
+              <List.Item>
+                Relatedness is determined by user provided weight / distance from
+                our 'root gene' (what gene was returned by the search)
+              </List.Item>
+            </List>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+
+      </Segment>
 
 
     </Responsive>
